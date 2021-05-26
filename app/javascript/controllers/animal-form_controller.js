b425c3ecfc936a669e561e12f19b6e96770b765f
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-05-26 14:10:11 -0300

    Changes Stimulus controller
    - Adds value to the data-controller
    - Changes the names of targets

--- app/javascript/controllers/animal-form_controller.js
+++ app/javascript/controllers/animal-form_controller.js
@@ -1,7 +1,25 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  initialize() {
-    console.log('Hello from stimulus!');
+  static targets = ['name', 'display', 'age', 'ageValidationMessage'];
+  static values = {counter: Number, ageValidation: Boolean};
+
+  countCharacters() {
+    this.counterValue = this.nameTarget.value.length;
+  }
+
+  validateAge() {
+    console.log(this.ageTarget.value)
+    this.ageValidationValue = this.ageTarget.value < 1
+  }
+
+  counterValueChanged() {
+    this.displayTarget.textContent = this.counterValue;
+  }
+
+  ageValidationValueChanged() {
+    console.log(this.ageValidationValue)
+    this.ageValidationValue === true ? this.ageValidationMessageTarget.textContent = 'Invalid age': 
+    this.ageValidationMessageTarget.textContent = ''
   }
 }
