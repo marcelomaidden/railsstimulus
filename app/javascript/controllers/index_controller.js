f133a6ac920417885a98eee758862a6615231162
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-05-29 12:04:24 -0300

    Adds table dinamycally using stimulus

--- app/javascript/controllers/index_controller.js
+++ app/javascript/controllers/index_controller.js
@@ -3,19 +3,42 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ['animalsJson'];
   
+  #createTd(content) {
+    let td = document.createElement('td');
+    td.innerHTML = content;
+    return td;   
+  }
+
+  #createTable(titles) {
+    let table = document.createElement('table');
+    let tHead = document.createElement('tHead');
+    let tr = document.createElement('tr');
+
+    titles.forEach(title => {
+      var name = document.createElement('th');
+      name.innerHTML = title;
+      tr.appendChild(name);
+    });
+    tHead.appendChild(tr);
+    table.appendChild(tHead);
+    return table;
+  }
+
   async fetchAnimals()
   {
     const spinner = document.querySelector('.spinner-border');
     spinner.classList.remove('d-none');
-    this.animalsJsonTarget.innerHTML = "<div class='font-weight-bold'>Name&nbsp;Age&nbsp;Actions<br/>";
+    const table = this.#createTable(['Name', 'Age', 'Actions']);
+    const tBody = document.createElement('tbody');
     setTimeout(() => {
       return fetch('http://localhost:3000/animals.json')
       .then(result => result.json())
-      .then(animals => { 
+      .then(animals => {
         animals.forEach(animal => {
-          this.animalsJsonTarget.innerHTML += `${animal.name}`;
-          this.animalsJsonTarget.innerHTML += `&nbsp;${animal.age}`;
-          this.animalsJsonTarget.innerHTML += `&nbsp;
+          var row = document.createElement("tr");
+          row.appendChild(this.#createTd(animal.name));
+          row.appendChild(this.#createTd(animal.age));
+          row.appendChild(this.#createTd(`&nbsp;
           <a href = 'animals/${animal.id}'>Show</a>
           <a href = 'animals/${animal.id}/edit'>Edit</a>
           <a 
@@ -24,9 +47,12 @@ export default class extends Controller {
             data-method="delete"
             href="/animals/${animal.id}">
               Destroy
-          </a>`;
-          this.animalsJsonTarget.innerHTML += '<br />';
+          </a>`));
+          tBody.appendChild(row);
         });
+        table.appendChild(tBody);
+        this.animalsJsonTarget.innerHTML = '';
+        this.animalsJsonTarget.appendChild(table);
         spinner.classList.add('d-none');
       });
     }, 1000);
