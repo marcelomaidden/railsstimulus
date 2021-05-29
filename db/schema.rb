31abec048bd253cb04788b26df83c11b1b303510
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-05-29 12:19:55 -0300

    Removes sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_26_160501) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "animals", force: :cascade do |t|
     t.string "name"
     t.integer "age"
